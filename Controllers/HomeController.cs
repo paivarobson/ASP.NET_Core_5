c95b1b41557e1b189e1ac00e93696404644c15a9
Robson Paiva <paivawork@hotmail.com>
2021-06-11 18:35:55 -0300

    Primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PrimeiroProjeto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index(int? id)
+        {
+            return View(id);
+        }
+    }
+}
